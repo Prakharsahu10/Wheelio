d77bded07c1f82429b9c38f2dca2d03aac621d01
Prakhar Sahu <prakhars762@gmail.com>
2025-06-28 22:52:48 +0530

    auth

---
+++ app/page.js
@@ -0,0 +1,12 @@
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div>
+      Hello World
+      <br />
+      <Button variant="destructive">Prakhar</Button>
+    </div>
+  );
+}
